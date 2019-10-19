5352eb2e2c7bc5226e23818a3356144d3865c012
nickbeaird <nbeaird@cloudboltsoftware.com>
2019-10-19 13:35:18 -0700

    Move all logic for runtime into object.

--- 01_datetime_pomodoro_clock/pomodoro_clock.py
+++ 01_datetime_pomodoro_clock/pomodoro_clock.py
@@ -27,16 +27,19 @@ class Pomodoro:
         return self.pomodoro_end
 
     def start_pomodoro(self):
+        print(f"Starting a Pomodoro of {pom.run_time} minutes.")
         self.pomodoro_start = dtm.now()
         self._set_pomodoro_end()
-        return self.pomodoro_start
+        self._run_pom_countdown()
 
     def _set_pomodoro_end(self):
         self.pomodoro_end = self.pomodoro_start + timedelta(minutes=self.run_time)
         return self.pomodoro_end
 
     def remaining_pomodoro_time(self):
-        return self.pomodoro_end - dtm.now()
+        time_remaining = self.pomodoro_end - dtm.now()
+        print(f"Keep your focus. You have {time_remaining} remaining in this Pomodoro.")
+        return time_remaining
 
     def all_done(self):
         return self.breaks == 4
@@ -44,9 +47,16 @@ class Pomodoro:
     def start_break(self):
         print("Starting break")
         self.checkmarks += 1
+        print(f"You have {self.checkmarks} checkmarks.")
+        self._set_break_start_time()
+        break_time = self.which_break()
+        print(f"running a break for {break_time}")
+        self._run_break_countdown()
         self.breaks += 1
+        print(f"You have taken {self.breaks} breaks")
+    
+    def _set_break_start_time(self):
         self.break_start_time = dtm.now()
-        self.which_break()
         return self.break_start_time
 
     def which_break(self):
@@ -71,6 +81,22 @@ class Pomodoro:
         remainder = self.break_end_time - dtm.now()
         return remainder
 
+    def _run_pom_countdown(self):
+        while True:
+            time_remaining = self.remaining_pomodoro_time()
+            print(f"The time remaining is {time_remaining}")
+            time.sleep(5)
+            if time_remaining <= timedelta(0, 5, 0):
+                break
+
+    def _run_break_countdown(self):
+        while True:
+            break_time_remaining = self.remaining_break_time()
+            print(f"The break time remaining is {break_time_remaining}")
+            time.sleep(5)
+            if break_time_remaining <= timedelta(0, 5, 0):
+                break
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="POMODORO")
@@ -92,30 +118,7 @@ if __name__ == "__main__":
 
         if usr_input in ["y", "yes"]:
             pom.start_pomodoro()
-            time_remaining = pom.remaining_pomodoro_time()
-            print(f"Starting a Pomodoro of {pom.run_time} minutes")
-            print(
-                f"The time now is {pom.pomodoro_start}, and you have until {pom.pomodoro_end}. This is the total of {time_remaining} seconds."
-            )
-
-            while True:
-                time_remaining = pom.remaining_pomodoro_time()
-                print(f"The time remaining is {time_remaining}")
-                time.sleep(5)
-                if time_remaining <= timedelta(0, 5, 0):
-                    break
-
             pom.start_break()
-            print("Starting break")
-            print(
-                f"The number of checkmarks is {pom.checkmarks} and the number of breaks is {pom.breaks}"
-            )
-            while True:
-                break_time_remaining = pom.remaining_break_time()
-                print(f"The break time remaining is {break_time_remaining}")
-                time.sleep(5)
-                if break_time_remaining <= timedelta(0, 5, 0):
-                    break
-
+            
         if pom.all_done() == True:
             break
